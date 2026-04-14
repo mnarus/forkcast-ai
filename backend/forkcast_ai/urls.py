6c66c2b7633cc43f4444f3ff7cd97afdbffa800f
Michelle <bereczgator@gmail.com>
2026-04-14 15:57:53 -0400

    add hello world endpoint, confirm FE and BE are talking

--- backend/forkcast_ai/urls.py
+++ backend/forkcast_ai/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import hello_world
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/hello/', hello_world, name='hello-world'),
 ]
